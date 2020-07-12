01cf279309b2fc005e5bb1f544e49f218fe232fc
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-12 15:45:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+  if speed< 40
+    return "True"
+    elsif speed > 60 "True"
+  else
+    return "False"
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+  speed <40 && > 60 ? "True" : "False"
 end
-	
-
+  
 
